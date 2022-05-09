26a496d570a2e2117f0f3ba95e6a03fa381029b2
morclaw <cong.207ct27643@vanlanguni.vn>
2022-05-09 16:19:40 +0700

    update

---
+++ 3PsProj/Controllers/FileReaderController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _3PsProj.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _3PsProj.Controllers
+{
+    [Route("api/reader/[action]")]
+    public class FileReaderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var stream = new FileStream("config.json", FileMode.OpenOrCreate);
+            var reader = new StreamReader(stream);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetFileReader()
+        {
+            int id = 1;
+            DirectoryInfo directory = new DirectoryInfo(@"ActionFolder");
+            DirectoryInfo[] folders = directory.GetDirectories("*", SearchOption.AllDirectories);
+            List<FileReader> fileReaders = new List<FileReader>();
+            foreach (DirectoryInfo folder in folders)
+            {
+                id++;
+                FileReader fileReader = new FileReader();
+                fileReader.Id = id;
+                fileReader.FileName = folder.Name;
+                fileReader.Type = false;
+                fileReader.Size = 0;
+                fileReader.Childrens = new List<FileReader>();
+                foreach (FileInfo file in folder.GetFiles("*.png", SearchOption.AllDirectories))
+                {
+                    FileReader fileReaderChild = new FileReader();
+                    fileReaderChild.Id = id;
+                    fileReaderChild.FileName = file.Name;
+                    fileReaderChild.Type = true;
+                    fileReaderChild.Size = (int)file.Length;
+                    fileReader.Childrens.Add(fileReaderChild);
+                }
+
+                fileReaders.Add(fileReader);
+            }
+            return Ok(fileReaders);
+        }
+    }
+}
---
+++ 3PsProj/Models/FileReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3PsProj.Models
+{
+    public class FileReader
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public bool Type { get; set; }
+        public int Size { get; set; }
+        public List<FileReader> Childrens { get; set; }
+    }
+}
